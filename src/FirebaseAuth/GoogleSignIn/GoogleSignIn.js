6e8164975014f6b429ee4b5d8e580035e175d242
= <=>
2021-10-19 08:04:20 -0400

    Online Doctor Updated

--- src/FirebaseAuth/GoogleSignIn/GoogleSignIn.js
+++ src/FirebaseAuth/GoogleSignIn/GoogleSignIn.js
@@ -2,14 +2,12 @@ import { getAuth, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
 import { useState } from "react";
 import FirebaseInit from "../FirebaseInit";
 const GoogleSignIn = ()=>{
-    const [user,setUser] = useState({})
     console.log("Clicked");
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
     signInWithPopup(auth, provider)
     .then(result=>{
         console.log(result.user);
-        setUser(result.user);
     }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
@@ -21,7 +19,6 @@ const GoogleSignIn = ()=>{
         const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
       });
-      return user;
 }
 
 export default GoogleSignIn;
